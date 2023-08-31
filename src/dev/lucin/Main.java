5612b59cd8e91892b0838dd0a029fd4026f1c6ab
Lucas Santiago Santana <lucin189@gmail.com>
2023-08-30 21:54:52 -0300

    feat(Main.java): shuffle deck before starting game

--- src/dev/lucin/Main.java
+++ src/dev/lucin/Main.java
@@ -1,14 +1,13 @@
 package dev.lucin;
 
 import dev.lucin.domain.Card;
-import dev.lucin.domain.Color;
+
+import java.util.Collections;
 
 public class Main {
-    private static Color tableColor;
-
     public static void main(String[] args) {
         var deck = Card.getFullDeck();
         Card.printDeck(deck);
-        System.out.println("Uno Deck Length = " + deck.size());
+        Collections.shuffle(deck);
     }
 }
