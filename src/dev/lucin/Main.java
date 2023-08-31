cd1a33014be38b7d0e18e79805a70887bf7356a9
Lucas Santiago Santana <lucin189@gmail.com>
2023-08-30 21:21:35 -0300

    feat(Main.java): add call to Card.printDeck and show deck.size

--- src/dev/lucin/Main.java
+++ src/dev/lucin/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         var deck = Card.getFullDeck();
-        System.out.println("Uno Deck = " + deck);
+        Card.printDeck(deck);
+        System.out.println("Uno Deck Length = " + deck.size());
     }
 }
