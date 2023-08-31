f8b0561e24cef09d636e93e2b96901ed51bb18a9
Lucas Santiago Santana <lucin189@gmail.com>
2023-08-30 21:54:11 -0300

    feat(Table.java): implement addCard and isValidCard methods

--- src/dev/lucin/domain/Table.java
+++ src/dev/lucin/domain/Table.java
@@ -1,2 +1,43 @@
-package dev.lucin.domain;public class Table {
+package dev.lucin.domain;
+
+import java.util.*;
+
+public class Table {
+    private Color currentColor;
+    private final Deque<Card> discardPile = new ArrayDeque<>();
+
+    public Table() {
+    }
+
+    public void addCard(Card card) {
+        if (!isValidCard(card)) {
+            System.err.println("Invalid Card!");
+            return;
+        }
+
+        discardPile.addFirst(card);
+        currentColor = card.getColor();
+        System.out.println("Played Card: " + card);
+    }
+
+    private boolean isValidCard(Card card) {
+        if (discardPile.isEmpty()) {
+            return true;
+        }
+
+        Card topItem = discardPile.peekFirst();
+        if (topItem != null) {
+            return card.getValue().equals(topItem.getValue())
+                    || card.getColor().equals(currentColor);
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Current Color: (%s)
+                Last Played Card: (%s)%n""".formatted(currentColor, discardPile.peekFirst());
+    }
 }
