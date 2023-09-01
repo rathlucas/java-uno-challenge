a7e84255eed064de6c698c98079ba9f1a400c7e3
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-01 09:23:54 -0300

    feat(main): start add scanner input reading to main game loop

--- src/dev/lucin/Main.java
+++ src/dev/lucin/Main.java
@@ -5,10 +5,11 @@ import dev.lucin.domain.Player;
 import dev.lucin.domain.Table;
 
 import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static Table table = new Table();
     private static Scanner sc = new Scanner(System.in);
     private static boolean gameLoop = true;
 
@@ -16,15 +17,25 @@ public class Main {
         var deck = Card.getFullDeck();
         Collections.shuffle(deck);
 
-        Player player1 = new Player();
-        Player player2 = new Player();
+        Player player1 = new Player("Lucin");
+        Player player2 = new Player("Flavin");
 
-        Card.dealHand(deck, player1);
-        Card.dealHand(deck, player2);
+        Table table = new Table(List.of(player1, player2));
+        table.dealHands(deck);
+
 
         while (gameLoop) {
-            System.out.println("Your Hand: " + player1.getHand());
+            System.out.println("Your Hand: " + player1.hand());
+            System.out.println("Choose a Card To Play (Enter a Number)");
 
+            try {
+                int option = sc.nextInt();
+                System.out.println("Chosen Option: " + option);
+                gameLoop = false;
+            } catch (InputMismatchException e) {
+                System.err.println("Invalid Option, Try Again!");
+                sc.nextLine();
+            }
         }
     }
 }
