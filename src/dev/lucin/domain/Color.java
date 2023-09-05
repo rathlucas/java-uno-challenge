5883b1fe1b4026449a21a97494e41a2f1d745f54
Lucas Santiago Santana <lucin189@gmail.com>
2023-09-05 08:50:46 -0300

    feat(color): create getColor static enum method

--- src/dev/lucin/domain/Color.java
+++ src/dev/lucin/domain/Color.java
@@ -2,4 +2,15 @@ package dev.lucin.domain;
 
 public enum Color {
     RED, GREEN, BLUE, YELLOW, BLACK;
+
+    static Color getColor(int value) {
+        return switch (value) {
+            case 1 -> RED;
+            case 2 -> GREEN;
+            case 3 -> BLUE;
+            case 4 -> YELLOW;
+            case 5 -> BLACK;
+            default -> throw new IllegalStateException("Unexpected value: " + value);
+        };
+    }
 }
